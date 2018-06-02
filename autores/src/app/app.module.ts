3b5e70f09b8d93885d6c5428af15b069eb8cb687
Brayan Jimenez <brayan.jimenez@epn.edu.ec>
2018-06-02 11:40:08 -0500

    instalacion de bootstrap y carrusel

--- autores/src/app/app.module.ts
+++ autores/src/app/app.module.ts
@@ -1,15 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+
 
 
 import { AppComponent } from './app.component';
+
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    NgbModule.forRoot(),
+    HttpClientModule
+
+
   ],
   providers: [],
   bootstrap: [AppComponent]
