6946c11e7347cc51b09135d69ba07c92facc20bd
Brayan Jimenez <brayan.jimenez@epn.edu.ec>
2018-06-03 23:52:43 -0500

    Examen

--- autores/src/app/app.component.ts
+++ autores/src/app/app.component.ts
@@ -1,6 +1,7 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnInit, Pipe} from '@angular/core';
 import {HttpClient} from '@angular/common/http';
-import {map} from 'rxjs/operators';
+import {count, map} from 'rxjs/operators';
+
 
 
 @Component({
@@ -10,8 +11,24 @@ import {map} from 'rxjs/operators';
 })
 
 
+
 export class AppComponent implements OnInit{
+
   images: Array<string>;
+  tit='asdf'
+  arregloBook = [
+    {
+      icbn:'235432',
+      nombre: 'la rayuela',
+      numeroPaginas: 120,
+      edicion: 'segunda',
+      fechaPublicacion: '1940',
+      nombreEditorial: 'x',
+      autorId:1
+    }
+  ];
+
+
 
   constructor(private _http: HttpClient) {}
 
@@ -19,12 +36,87 @@ export class AppComponent implements OnInit{
     this._http.get('https://picsum.photos/list')
       .pipe(map((images: Array<{id: number}>) => this._randomImageUrls(images)))
       .subscribe(images => this.images = images);
+
   }
 
   private _randomImageUrls(images: Array<{id: number}>): Array<string> {
+
     return [1, 2, 3].map(() => {
       const randomId = images[Math.floor(Math.random() * images.length)].id;
       return `https://picsum.photos/900/500?image=${randomId}`;
     });
   }
+    Titulo='Presione sobre algun autor';
+    CardTitulo='';
+    CardBody='';
+
+   arregloAutores = [
+     {
+  nombres: 'julio',
+  apellidos: 'cortazar',
+  fechaNacimiento: '1914',
+  numeroLibros: 50,
+  ecuatoriano: false
 }
+];
+
+
+
+  crearAutor (nombre,apellido,fecha,numero,ecuatoriano){
+   let autor = {
+      nombres: nombre,
+      apellidos: apellido,
+      fechaNacimiento: fecha,
+      numeroLibros: Number(numero),
+      ecuatoriano: ecuatoriano
+
+  }
+  this.arregloAutores.push(autor);
+    alert('Se creo el autor');
+   return this.arregloAutores;
+    }
+
+
+    llenarBoton(boton){
+    if (this.arregloAutores.length<=boton){
+      return 'Vacio'
+    }
+    else{
+      return this.arregloAutores[boton].nombres
+    }
+
+    }
+
+    llenarCarrusel(boton){
+    if (this.arregloAutores.length<=boton){
+      alert('Este boton esta vacio')
+    }
+    else
+    {this.Titulo = this.arregloAutores[boton].nombres+' '+ this.arregloAutores[boton].apellidos}
+    }
+
+  crearLibros (icbnL,nombreL,numeroPaginaL,edicionL,fechaL,editorialL,autorL){
+    let libro = {
+      icbn: icbnL,
+      nombre: nombreL,
+      numeroPaginas: numeroPaginaL,
+      edicion: edicionL,
+      fechaPublicacion: fechaL,
+      nombreEditorial: editorialL,
+      autorId:autorL
+    }
+    this.arregloBook.push(libro)
+    alert('Se creo el libro')
+    return this.arregloBook
+  }
+
+  llenarCard (){
+    this.tit = document.getElementById("celda").innerText
+  }
+
+}
+
+
+
+
+
--- autores/src/app/app.module.ts
+++ autores/src/app/app.module.ts
@@ -2,7 +2,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 import { FormsModule } from '@angular/forms';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import { HttpClientModule } from '@angular/common/http';
+import {MatButtonModule, MatCheckboxModule} from '@angular/material';
+import {MatInputModule} from '@angular/material/input';
+import {ButtonModule} from 'primeng/button';
+import {CardModule} from 'primeng/card';
+import {TableModule} from 'primeng/table';
+
+
+
+
 
 
 
@@ -17,7 +27,15 @@ import { AppComponent } from './app.component';
     BrowserModule,
     FormsModule,
     NgbModule.forRoot(),
-    HttpClientModule
+    HttpClientModule,
+    BrowserAnimationsModule,
+    MatButtonModule, MatCheckboxModule,
+    MatInputModule,
+    ButtonModule,
+    CardModule,
+    TableModule,
+
+
 
 
   ],
