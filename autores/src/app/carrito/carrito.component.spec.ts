8337cfbe98631ca4dcff9e4ffaaa7fc2e24dba10
brayanjg <brayan-andriu@hotmail.com>
2018-07-11 08:50:09 -0500

    creacion de grids y estructura de las paginas

--- autores/src/app/carrito/carrito.component.spec.ts
+++ autores/src/app/carrito/carrito.component.spec.ts
@@ -2,6 +2,7 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { CarritoComponent } from './carrito.component';
 
+
 describe('CarritoComponent', () => {
   let component: CarritoComponent;
   let fixture: ComponentFixture<CarritoComponent>;
--- autores/src/app/carrito/carrito.component.ts
+++ autores/src/app/carrito/carrito.component.ts
@@ -1,15 +1,32 @@
 import { Component, OnInit } from '@angular/core';
-
+import {NgbModal, ModalDismissReasons} from '@ng-bootstrap/ng-bootstrap';
 @Component({
   selector: 'app-carrito',
   templateUrl: './carrito.component.html',
   styleUrls: ['./carrito.component.css']
 })
 export class CarritoComponent implements OnInit {
+  closeResult: string;
 
-  constructor() { }
+  constructor( private modalService: NgbModal) { }
 
   ngOnInit() {
   }
+  open(content) {
+    this.modalService.open(content, {}).result.then((result) => {
+      this.closeResult = `Closed with: ${result}`;
+    }, (reason) => {
+      this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
+    });
+  }
 
+  private getDismissReason(reason: any): string {
+    if (reason === ModalDismissReasons.ESC) {
+      return 'by pressing ESC';
+    } else if (reason === ModalDismissReasons.BACKDROP_CLICK) {
+      return 'by clicking on a backdrop';
+    } else {
+      return  `with: ${reason}`;
+    }
+  }
 }
